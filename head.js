799d347d38a4b826b7f3988ad48f914ec51be034
Brittany Lasala <brittanylasala@gmail.com>
2019-10-10 20:25:46 +0000

    created a head function that returns the head of an array

--- head.js
+++ head.js
@@ -7,6 +7,9 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("hello", "hello");
-assertEqual(3, 1);
+let head = function(arr) {
+  return arr[0]
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
